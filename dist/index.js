9ef8efde4346b07708a3aaf0b00d3e9308502218
Mikhail <mihail.kuznetsov.99@mail.ru>
2022-09-06 18:18:31 +0300

    work animete and hide

--- dist/index.js
+++ dist/index.js
@@ -2,20 +2,28 @@ const circle = document.querySelector('.progress-circle');
 const radius = circle.r.baseVal.value; 
 const circumference = 2 * Math.PI * radius;
 const input = document.querySelector('.percent')
+const hide = document.getElementById('hide')
+const animate = document.getElementById('animate')
 
 circle.style.strokeDasharray = `${circumference} ${circumference}`;
 circle.style.strokeDashoffset = circumference;
 
+let isHidden = false;
+let isAnimated = false;
 
-function setProgress(persent) {
-    const offset = circumference - persent / 100 * circumference;
+function setProgress(percent) {
+    const offset = circumference - percent / 100 * circumference;
     circle.style.strokeDashoffset = offset;
 }
 
-input.addEventListener('change', () => {
-    if (input.value <= 100) {
-        setProgress(input.value)
-    }else {
-        setProgress(100)  
-    }
-});
+input.addEventListener('change', () => input.value <= 100 ? setProgress(input.value) : setProgress(100));
+
+hide.addEventListener('click', () => {
+   isHidden = !isHidden;
+   isHidden === true ? circle.style.display = 'none' : circle.style.display = 'flex';
+}); 
+
+animate.addEventListener('click', () => {
+    isAnimated = !isAnimated;
+    isAnimated === true ? circle.classList.add('animate') : circle.classList.remove('animate');
+ }); 
