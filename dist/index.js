5f9672e0c2365db007636372ed2b9512aafac83a
Mikhail <mihail.kuznetsov.99@mail.ru>
2022-09-10 14:41:45 +0300

    new class render

--- dist/index.js
+++ dist/index.js
@@ -1,59 +1,119 @@
-const circle = document.querySelector('.progress-circle');
-const radius = circle.r.baseVal.value; 
-const circumference = 2 * Math.PI * radius;
-const percentInput = document.getElementById('percent')
-const hideInput = document.getElementById('hide')
-const animateInput = document.getElementById('animate')
-const circleContainer = document.querySelector('.progress-container')
+class UiProgress extends HTMLElement {
 
-circle.style.strokeDasharray = `${circumference} ${circumference}`;
-circle.style.strokeDashoffset = circumference;
+    get value() {
+        return this.getAttribute('value');
+    }
 
-class Progress {
-    isHidden = false;   
-    isAnimated = false;
+    set value(val) {
+        this.setAttribute("value", val)
+    }
 
-    setValue(percent) {
+    static get observedAttributes() {
+        return ["value"]
+    }
+
+    attributeChangedCallback(prop, oldValue, newValue) {
+        if(prop === "value") this.render();
+    }
+
+    setValue(percent, circumference, circle) {
         const offset = circumference - percent / 100 * circumference;
         circle.style.strokeDashoffset = offset;
     }
 
-    setValueLimit(percent) {
-        if ( percent <= 100) {
-            this.setValue(percent)
+    setValueLimit(percent, circumference, circle) {
+        console.log(percent);
+        if ( percent <= 100 ) {
+            this.setValue(percent, circumference, circle)
         }else {
-            percentInput.value = 100;
-            this.setValue(percentInput.value)
+            this.setValue( 100, circumference, circle)
         }
     }
 
-    toggleHiddenValue() {
-        this.isHidden = !this.isHidden;
+    setAnimationValue(circle) {
+        circle.classList.toggle('animate');
     }
 
-    toggleAnimatedValue() {
-        this.isAnimated = !this.isAnimated;
+    setHiddenValue(circleContainer) {
+        circleContainer.classList.toggle('hidden');
     }
 
-    setHiddenValue() {
-        this.toggleHiddenValue();
-        this.isHidden ? circleContainer.classList.add('hidden') : circleContainer.classList.remove('hidden');
+    connectedCallback() {
+        this.render();
+        
+        const section = this.children[0];    
+        const circle = section.querySelector('.progress-circle');
+        const radius = circle.r.baseVal.value; 
+        const circumference = 2 * Math.PI * radius;
+        const percentInput = section.querySelector('.control-button-percent')
+        const hideInput = section.querySelector('.hide')
+        const animateInput = section.querySelector('.animated')
+        const circleContainer = section.querySelector('.progress-container')
+
+        const offset = circumference - percentInput.value / 100 * circumference;
+        circle.style.strokeDashoffset = offset;
+        circle.style.strokeDasharray = `${circumference} ${circumference}`;
+      
+        percentInput.addEventListener('input', () => {
+            percentInput.value = percentInput.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+            this.setValueLimit(percentInput.value, circumference, circle);
+        });
+
+        animateInput.addEventListener('click', () => this.setAnimationValue(circle)); 
+        hideInput.addEventListener('click', () => this.setHiddenValue(circleContainer));
+
     }
 
-    setAnimationValue() {
-        this.toggleAnimatedValue();
-        this.isAnimated ? circle.classList.add('animate') : circle.classList.remove('animate');
+    render() {
+        this.innerHTML = ` 
+        <section>
+            <h1>Progress</h1>
+            <div class="main-wrapper">
+                <div class="progress-wrapper">
+                    <div class="progress-container">
+                        <div class="outer">
+                            <div class="inner"></div>
+                        </div>
+                
+                        <svg class="progress-ring" width="180" height="180">
+                            <defs>
+                                <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                                <stop offset="0%" stop-color="#0062FF" />
+                                <stop offset="100%" stop-color="#5eaefd" />
+                                </linearGradient>
+                            </defs>
+                            <circle 
+                            class="progress-circle"
+                            stroke="url(#gradient)"
+                            stroke-width="15" 
+                            cx="90" cy="90" r="82" 
+                            fill="transparent"
+                            />
+                        </svg>
+                    </div>
+                </div>
+                <div class="control-buttons-wrapper">
+                    <div class="control-button-container">
+                        <input 
+                        type="text" 
+                        class="control-button-percent" 
+                        value="${this.value}" 
+                        maxlength="3"
+                        >
+                        <label class="control-button-title">Value</label>
+                    </div>
+                    <div class="control-button-container">
+                        <input type="checkbox" class="control-button-checkbox animated">
+                        <label class="control-button-title">Animate</label>
+                    </div>
+                    <div class="control-button-container">
+                        <input type="checkbox" class="control-button-checkbox hide">
+                        <label class="control-button-title">Hide</label>
+                    </div>
+                </div>
+            </div>
+        </section>` 
     }
-
 }
 
-const progress = new Progress;
-
-percentInput.addEventListener('input', () => {
-    percentInput.value = percentInput.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
-    progress.setValueLimit(percentInput.value);
-});
-
-hideInput.addEventListener('click', () => progress.setHiddenValue()); 
-
-animateInput.addEventListener('click', () => progress.setAnimationValue()); 
+customElements.define('ui-progress', UiProgress)
